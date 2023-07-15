a82edef49e066702f0dedc8dcd439e80a97c19f1
Pom-Veerawat <118708779+Pom-Veerawat@users.noreply.github.com>
2023-07-15 13:50:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import { ScrollContainer, SequenceSection } from "react-nice-scroll";
 import "react-nice-scroll/dist/styles.css";
 function App() {
+  const currentURL = window.location.href; // returns the absolute URL of a page
+
+  const pathname = window.location.pathname; //returns the current url minus the domain name
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,12 +37,15 @@ function App() {
           >
             <h1>Demo</h1>
           </section>
+          <div style={{ width:'200px', alignItems: "center",
+              justifyContent: "center",}}>
           <SequenceSection
             end="300%"
             imagesPath="/img2"
             imagesCount={101}
             imagesType="jpg"
           />
+          </div>
           <section
             style={{
               height: "100vh",
